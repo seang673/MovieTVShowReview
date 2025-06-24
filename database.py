4398e0426b1ef9510bbacea6f4cc37621b82910b
seang673 <seangriffiths673@gmail.com>
2025-06-24 14:39:18 -0400

    Database Changes

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import psycopg2
+import os
 
 #Database connection settings
 
@@ -11,11 +12,7 @@ DB_PORT = "5432"
 #Establish connection
 
 conn = psycopg2.connect(
-    dbname = DB_NAME,
-    user = DB_USER,
-    password = DB_PASSWORD,
-    host = DB_HOST,
-    port = DB_PORT
+    os.environ["DATABASE_URL"]
 )
 
 # Create a cursor to execute SQL queries
