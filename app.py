9f29a31b3053cf1f1157fdd8ca5d13ee40575f6e
seang673 <seangriffiths673@gmail.com>
2025-07-12 14:37:02 -0400

    app.py change

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['WTF_CSRF_SECRET_KEY'] = os.getenv("CSRF_SECRET_KEY")
 
